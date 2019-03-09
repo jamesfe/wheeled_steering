3e97a695a4e8fd47360cd712a2c11b7ebb824b84
jamesfe <james.ferrara@gmail.com>
2019-03-09 13:34:28 +0100

    Websocket client

---
+++ webapp/main.js
@@ -0,0 +1,71 @@
+console.log('hi');
+
+class Comms {
+
+  constructor() {
+    this.socket = undefined;
+    this.connected = false;
+  }
+
+  connect() {
+    if (!this.connected) {
+      console.log('connecting');
+      this.socket = new WebSocket("ws://127.0.0.1:9001/control_socket");
+      this.socket.addEventListener('open', function (event) {
+          console.log('Open for business!');
+          setConnected();
+          this.send('Hello Server!');
+      });
+    console.log('connected');
+    }
+  }
+
+  send(message) {
+    if (!this.connected) {
+      this.connect();
+    }
+    let json_msg = JSON.stringify(message);
+    messageLog.innerHTML = messageLog.innerHTML + "<br>" + json_msg;
+    console.log('sending message: ' + json_msg);
+    this.socket.send(json_msg);
+  }
+}
+
+var commo = new Comms();
+var global_connected = false;
+
+let setConnected = function() {
+  console.log('Connecting to websocket!');
+  commo.connected = true;
+  global_connected = true;
+};
+
+function initial_connect() {
+  commo.connect();
+}
+
+
+let messageLog = document.getElementById('messageLog');
+document.addEventListener('keydown', handleKeys);
+
+function handleKeys(event) {
+  if (global_connected) {
+    switch (event.key) {
+      case ' ':
+        commo.send({'message': 'stop'});
+        break;
+      case 'i':
+        commo.send({'message': 'faster'});
+        break;
+      case 'k':
+        commo.send({'message': 'slower'});
+        break;
+      case 'j':
+        commo.send({'message': 'left'});
+        break;
+      case 'l':
+        commo.send({'message': 'right'});
+        break;
+    }
+  }
+}
